26c7f41a62d7dd63328f3e18a338d978d8d7795f
EmilOtisRosanowski <s6emrosa@uni-bonn.de>
2026-03-10 16:51:04 +0100

    Include Test Schwinger

--- TestSchwinger.py
+++ TestSchwinger.py
@@ -7,29 +7,30 @@ from scipy.sparse import csr_matrix
 
 # Random Hamiltonian
 
-K = 5
-U = 7.0
-t = 1.0
-V = 1.0
+N = 14
+x = (N/30)**2
+lam = 1000
+l0 = 2
+m_lat = 10.0
+g = 1.0
+H = generateSchwingerHamiltonian(N, x, lam, l0, m_lat, g)
 
-H, eps, Vk = siam_diagonal_bath_sparse_pauli(K=K, U=U, t=t, V=V)
+initial_state = '10' * (N//2)
 
-psi0, occ = siam_bitstring_initial_state(K=K)
-
-initial_state = ''.join(str(b) for b in occ)
-
-BK = BARK(H, initial_state, max_iterations=100, time_step=1, tolerance=0.01)
+BK = BARK(H, initial_state, max_iterations=100, time_step=1, tolerance=0.001)
 
 bases = BK.basis
 
 energy_est = []
 
-correct_energy = primme.eigsh(csr_matrix(H.to_matrix()), k=1, which='SA', tol=1e-6)[0][0]
+correct_energy = primme.eigsh(csr_matrix(H.to_matrix()), k=1, which='SA', tol=1e-10)[0][0]
+
+print("Exact ground state energy:", correct_energy)
 
 for basis in bases:
     print("Size of basis:", len(basis))
     H_p = BK.project_to_subspace(basis)
-    w,v = primme.eigsh(H_p, k=1, which='SA', tol=1e-6)
+    w,v = primme.eigsh(H_p, k=1, which='SA', tol=1e-10)
     energy_est.append(w[0])
     print("Estimated ground state energy:", w[0])
     print("Error:", abs(w[0] - correct_energy))
